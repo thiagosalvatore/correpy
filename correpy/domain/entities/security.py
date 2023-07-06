02688463966ee4350cc0e7d4fa92435313a30d02
alexsilva <geniofuturo@gmail.com>
2023-07-06 12:00:27 -0300

    Addition of code for subscription.

--- correpy/domain/entities/security.py
+++ correpy/domain/entities/security.py
@@ -8,7 +8,7 @@ from typing import Optional
 # 6	Preferenciais Classe B / ELET6
 # 11 BDRs, ETs e Units / BOVA11
 
-BASIC_TICKER_PATTERN = "([A-Z-0-9]{4})(3|4|5|6|11)(F|B)?"  # Format XXXXY or XXXXYF or XXXXYB where Y can be 3, 4, 11
+BASIC_TICKER_PATTERN = "([A-Z-0-9]{4})(3|4|5|6|11|12)(F|B)?"  # Format XXXXY or XXXXYF or XXXXYB where Y can be 3, 4, 11
 BDR_TICKER_PATTERN = "([A-Z-0-9]{4})(31|32|33|34|35|36|39)"  # Format XXXXYY where YY can be 31, 32, 33, 34, 35, 36, 39
 
 
